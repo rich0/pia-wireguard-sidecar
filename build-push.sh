f342e45e60f84c91405b38f11e5fa058f2e34ce1
Richard Freeman <rich0@gentoo.org>
2026-07-03 07:54:48 -0400

    bump
    Signed-off-by: Richard Freeman <rich0@gentoo.org>

--- build-push.sh
+++ build-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ALPINE_RELEASE=3.22
+ALPINE_RELEASE=3.24
 docker build . --pull --no-cache --build-arg ALPINE_RELEASE=$ALPINE_RELEASE --tag ghcr.io/rich0/pia-wireguard-sidecar:$ALPINE_RELEASE --tag ghcr.io/rich0/pia-wireguard-sidecar:latest && docker push ghcr.io/rich0/pia-wireguard-sidecar:$ALPINE_RELEASE && docker push ghcr.io/rich0/pia-wireguard-sidecar:latest
 #docker build . --pull --build-arg ALPINE_RELEASE=$ALPINE_RELEASE --tag ghcr.io/rich0/pia-wireguard-sidecar:$ALPINE_RELEASE --tag ghcr.io/rich0/pia-wireguard-sidecar:latest && docker push ghcr.io/rich0/pia-wireguard-sidecar:$ALPINE_RELEASE && docker push ghcr.io/rich0/pia-wireguard-sidecar:latest
 
