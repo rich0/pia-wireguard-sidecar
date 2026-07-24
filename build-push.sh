138011b9852e6a60589b038ae4114533d5a770f7
Richard Freeman <rich@rich0.org>
2026-07-24 17:38:10 -0400

    tag previous image
    Signed-off-by: Richard Freeman <rich@rich0.org>

--- build-push.sh
+++ build-push.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 ALPINE_RELEASE=3.24
 IMAGE=registry.rich0.org/public/pia-wireguard-sidecar
+regctl image copy  "${IMAGE}:latest" "${IMAGE}:previous"
 docker build . --pull --no-cache --build-arg ALPINE_RELEASE=$ALPINE_RELEASE \
   --tag "$IMAGE:$ALPINE_RELEASE" --tag "$IMAGE:latest"
 docker push "$IMAGE:$ALPINE_RELEASE"
